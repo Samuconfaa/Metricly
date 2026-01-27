19f811b137f427e12b5b77e37750f933c4e2453b
Samuconfaa <samuconfa08@gmail.com>
2026-01-27 22:43:29 +0100

    initial commit

---
+++ Metricly/Core/LenghtMeasure.cs
@@ -0,0 +1,33 @@
+﻿namespace Metricly.Core
+{
+    public class LenghtMeasure
+    {
+        public double BaseValue { get; }
+
+        public LenghtMeasure(double baseValue)
+        {
+            BaseValue = baseValue;
+        }
+        public double To(double factor) => BaseValue / factor;
+    }
+
+    public static class LenghtExtension
+    {
+        private const double Meter = 1.0;
+        private const double Kilometer = 1000.0;
+        private const double Centimeter = 0.01;
+        private const double Inch = 0.0254;
+
+        public static LenghtMeasure Meters(this double value) => new LenghtMeasure(value * Meter);
+        public static LenghtMeasure Kilometers(this double value) => new LenghtMeasure(value * Kilometer);
+        public static LenghtMeasure Inches(this double value) => new LenghtMeasure(value * Inch);
+        public static LenghtMeasure Centimetres(this double value) => new LenghtMeasure(value * Centimeter);
+
+
+        public static double ToMeters(this LenghtMeasure m) => m.To(Meter);
+        public static double ToKilometers(this LenghtMeasure m) => m.To(Kilometer);
+        public static double ToInches(this LenghtMeasure m) => m.To(Inch);
+        public static double ToCentimetres(this LenghtMeasure m) => m.To(Centimeter);
+
+    }
+}
---
+++ Metricly/Core/TempMeasure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metricly.Core
+{
+    public class TempMeasure
+    {
+        public double KelvinValue { get; }
+
+        public TempMeasure(double kelvinValue)
+        {
+            KelvinValue = kelvinValue;
+        }
+
+        public double To(double ratio, double offset) => (KelvinValue - offset) / ratio;
+    }
+
+    public static class TemperatureExtensions
+    {
+        private const double KelvinOffset = 0.0;
+        private const double CelsiusOffset = 273.15;
+        private const double FahrenheitOffset = 459.67;
+        private const double FahrenheitRatio = 5.0 / 9.0;
+
+
+        //chiamo il metodo con il valore in kelvin
+        public static TempMeasure Celsius(this double value)
+            => new TempMeasure(value + CelsiusOffset);
+
+        public static TempMeasure Fahrenheit(this double value)
+            => new TempMeasure((value + FahrenheitOffset) * FahrenheitRatio);
+
+        //grazie al valore in kelvin faccio poi la conversione
+        public static double ToCelsius(this TempMeasure m)
+            => m.KelvinValue - CelsiusOffset;
+
+        public static double ToFahrenheit(this TempMeasure m)
+            => (m.KelvinValue / FahrenheitRatio) - FahrenheitOffset;
+    }
+}
